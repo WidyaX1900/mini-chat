1300ac253a85330cabdd3dbc52d76169305956cf
WidyaX1900 <tuderangga93@gmail,com>
2025-08-10 13:17:16 +0800

    nodejs: import nodejs libraries

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const port = process.env.PORT || 3000;
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send(`<p>NodeJS running smoothly...</p>`);
+});
+
+io.on("connection", (socket) => {
+});
+
+
+server.listen(port, () => console.log(`NodeJS running on port: ${port}`));
