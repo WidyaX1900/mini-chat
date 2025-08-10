98e89efb2dd9f40d73d2dc41e5b8f372c88c18a7
WidyaX1900 <tuderangga93@gmail,com>
2025-08-10 14:19:31 +0800

    modified: url to show chatting page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,7 @@
                         <td>{{ $loop->iteration }}</td>
                         <td>{{ $user->name }}</td>
                         <td>
-                            <a href="/chat/{{ $user->id }}" class="btn btn-sm btn-primary me-2">
+                            <a href="/chatting/show/{{ $user->id }}" class="btn btn-sm btn-primary me-2">
                                 <i class="fa-solid fa-comment-dots"></i>
                             </a>
                         </td>
