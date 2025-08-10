3955293ff473920d2783f9b4a9438b302f4f8fab
WidyaX1900 <tuderangga93@gmail,com>
2025-08-10 14:25:24 +0800

    import: socket.io client, jquery and custom js scripts

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -9,18 +9,20 @@
     <title>Mini Chat App</title>
     <link rel="stylesheet" href="{{ config('app.asset_url') }}bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    @vite(['resources/js/app.js'])
+    @vite(['resources/js/app.js', 'resources/js/script.js'])
 </head>
 
 <body>
     @yield('navbar')
     @yield('content')
+    <script src="https://cdn.socket.io/4.8.1/socket.io.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
         integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js"
         integrity="sha384-7qAoOXltbVP82dhxHAUje59V5r2YsVfBafyUDxEdApLPmcdhBPg1DKg1ERo0BZlK" crossorigin="anonymous">
     </script>
+    <script src="{{ config('app.asset_url') }}js/jquery-3.7.1.js"></script>
 </body>
 
 </html>
