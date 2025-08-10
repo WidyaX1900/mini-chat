38969c701ad564b0ada99ccb79c03263d790aa8b
WidyaX1900 <tuderangga93@gmail,com>
2025-08-10 13:32:35 +0800

    db: make model, migration, controller for chatting data

---
+++ app/Http/Controllers/ChattingController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chatting;
+use App\Http\Requests\StoreChattingRequest;
+use App\Http\Requests\UpdateChattingRequest;
+
+class ChattingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreChattingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chatting $chatting)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chatting $chatting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateChattingRequest $request, Chatting $chatting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chatting $chatting)
+    {
+        //
+    }
+}
