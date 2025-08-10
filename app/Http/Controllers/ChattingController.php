c1cd77c3a221715097899b2f2e05d048bc14a3f5
WidyaX1900 <tuderangga93@gmail,com>
2025-08-10 13:36:51 +0800

    modified: rename controller as same as the model name

--- app/Http/Controllers/ChattingController.php
+++ app/Http/Controllers/ChattingController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Chatting;
 use App\Http\Requests\StoreChattingRequest;
 use App\Http\Requests\UpdateChattingRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ChattingController extends Controller
 {
@@ -13,7 +15,11 @@ class ChattingController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::select('id', 'name')
+            ->where('id', '!=', Auth::id())
+            ->get();
+
+        return view('index', ['users' => $users]);
     }
 
     /**
